a904b430967ffe7eb71f13f9c6638114a9bb4f3f
Ielciu Cristian <ielciu.cristian01@gmail.com>
2023-07-21 16:55:56 +0300

    Added spring data jpa service for vets. closes #25

--- sf-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/services/VetService.java
+++ sf-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/services/VetService.java
@@ -1,9 +1,7 @@
 package guru.springframework.services;
 
-import guru.springframework.model.Owner;
 import guru.springframework.model.Vet;
 
-import java.util.Set;
 
 public interface VetService extends CrudService<Vet,Long>{
 }
